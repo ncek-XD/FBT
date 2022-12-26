f9db3c9055a9fbb436628ff54a7068245a1fd421
NCEK <101446019+ncek-XD@users.noreply.github.com>
2022-12-26 16:25:24 +0700

    Create FBN.py

---
+++ FBN.py
@@ -0,0 +1,6 @@
+import os,rich
+from rich import print as prints
+os.system('git pull')
+prints('Sc FREE habis waktu')
+prints('Silahkan hubungi author untuk membeli sc')
+prints('Alasan sudah tidak free yaitu agar method tidak cepat mokad')
